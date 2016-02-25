9e6be53b4292faa4965e5e4e9ad2d93323ce515b
josephgarland <josephwgarland57@gmail.com>
2016-02-24 18:49:41 -0800

    first commit

---
+++ day1/app01.js
@@ -0,0 +1,20 @@
+/*   JOSEPH GARLAND
+	 JAVASCRIPT 1 
+	 2-22-2016  
+*/
+
+
+
+"use strict";
+
+var num1 = 12
+var num2 = 0x1E
+
+var a = "answer equals"
+
+var num3 = num1 + num2;
+
+
+var answer = a + " " + num3;
+/*var answer = a.concat(num3); */
+
---
+++ day1/day1.js
@@ -0,0 +1,35 @@
+// GEEKWISE JS1 - DAY 1 NOTES 
+// JOSEPH GARLAND
+// 2/22/2016
+
+
+"use strict"; 
+
+//alert("hello world");
+
+var food = "pizza"
+var drink ="iced tea"
+
+var foodanddrink = food + ", " + drink;
+
+console.log(foodanddrink);
+
+
+var num1 = 5
+var num2 = 10
+var num3 = 20
+
+var sum = num1 + num2 + num3;
+
+var average = sum / 3;   // forward slash is division****
+
+
+
+console.log(sum);
+console.log(average);
+
+
+var numArray = [5, 10, 20];
+sum = numArray[0] + numArray[1] + numArray[2];
+
+console.log(sum);
